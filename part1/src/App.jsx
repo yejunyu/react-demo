15112c2bafb5942aeae8121c8fbf78cbef3503b8
yjy <yyyejunyu@gmail.com>
2024-02-18 17:02:16 +0800

    https://fullstackopen.com/en/part1/a_more_complex_state_debugging_react_apps 作业

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,30 +1,50 @@
 import { useState } from "react";
 
-const Display = ({ counter }) => {
-  return <div>{counter}</div>;
-};
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
 
-const Button = (props) => {
-  return <button onClick={props.onClick}>{props.text}</button>;
-};
+  const [selected, setSelected] = useState(0);
+  const [voteArr, setVoteArr] = useState(Array.from({ length: 10 }, () => 0));
+  const [mostVoted, setMostVoted] = useState(anecdotes[0]);
 
-const App = (props) => {
-  const [counter, setCounter] = useState(0);
-  console.log("rendering with counter...", counter);
-  const increaseByOne = () => {
-    console.log("increase with counter...", counter);
-    setCounter(counter + 1);
+  const findMaxNumber = (arr) => {
+    const maxNumber = Math.max(...arr);
+    return arr.indexOf(maxNumber);
   };
-  const zetToZero = () => {
-    console.log("zero with counter...", counter);
-    setCounter(0);
+
+  const voteHandler = (params) => {
+    const copy = [...voteArr];
+    copy[selected] += 1;
+    setVoteArr(copy);
+    const mostVotedAnecdotes = anecdotes[findMaxNumber(copy)];
+    console.log(mostVotedAnecdotes);
+    setMostVoted(mostVotedAnecdotes);
   };
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="plus"></Button>
-      <Button onClick={zetToZero} text="zero"></Button>
+      {anecdotes[selected]}
+      <div>has {voteArr[selected]} votes</div>
+      <br />
+      <button onClick={voteHandler}>vote</button>
+      <button
+        onClick={() =>
+          setSelected(Math.floor(Math.random() * anecdotes.length))
+        }
+      >
+        next
+      </button>
+      <h1>most voted</h1>
+      <span>{mostVoted}</span>
     </div>
   );
 };
